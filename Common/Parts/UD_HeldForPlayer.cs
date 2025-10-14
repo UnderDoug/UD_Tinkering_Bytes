2befc121bd4a834f9a7730d7b05f835b3c42a5ee
UnderDoug <douglas.marken@gmail.com>
2025-10-14 22:20:21 +1000

    adjusted usings.

--- Common/Parts/UD_HeldForPlayer.cs
+++ Common/Parts/UD_HeldForPlayer.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Qud.UI;
 using XRL.Language;
 using XRL.UI;
 using XRL.World.Capabilities;
@@ -10,7 +11,6 @@ using XRL.World.Tinkering;
 
 using static XRL.World.Parts.Skill.Tinkering;
 
-
 using UD_Vendor_Actions;
 using UD_Modding_Toolbox;
 
@@ -18,7 +18,7 @@ using static UD_Modding_Toolbox.Const;
 
 using UD_Tinkering_Bytes;
 using static UD_Tinkering_Bytes.Options;
-using Qud.UI;
+
 using SerializeField = UnityEngine.SerializeField;
 
 namespace XRL.World.Parts
